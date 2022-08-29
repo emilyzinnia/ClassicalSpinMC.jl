908d13090bd58bc8efce3691c8e0aad701e76c2a
emilyzinnia <ezhang@physics.utoronto.ca>
2022-08-29 17:17:47 -0400

    fix printer

--- src/helper.jl
+++ src/helper.jl
@@ -23,27 +23,34 @@ function MPISimpleRecvSend(sendbuf::Real, dest::Integer, comm::MPI.Comm)
 end
 
 function print_runtime_statistics!(mc, output_stats::Vector{Int64}, enableMPI::Bool)
-    rank = 0 
     accepted_local, exchange_rate, exchange_rate_prev, s_prev, local_prev = output_stats
     total_sweeps = mc.parameters.t_thermalization + mc.parameters.t_measurement
     t = mc.sweep
     progress = 100.0 * mc.sweep / total_sweeps 
     thermalized = (mc.sweep >= mc.parameters.t_thermalization) ? "YES" : "NO"
     attempted_local = (t - s_prev) * mc.lattice.size / mc.parameters.OR
-    attempted_swap = (rank == 0 || rank == commSize - 1) ? (t - s_prev) / mc.parameters.swap_rate / 2.0 : (t - s_prev) / mc.parameters.swap_rate
     local_acceptance_rate = (accepted_local-local_prev) / attempted_local * 100.0
-    exchange_rate_tot = (exchange_rate-exchange_rate_prev) / attempted_swap * 100.0
 
     if enableMPI
         comm = MPI.COMM_WORLD
         commSize = MPI.Comm_size(comm)
         rank = MPI.Comm_rank(comm)
+
+        attempted_swap = (rank == 0 || rank == commSize - 1) ? (t - s_prev) / mc.parameters.swap_rate / 2.0 : (t - s_prev) / mc.parameters.swap_rate
+        exchange_rate_tot = (exchange_rate-exchange_rate_prev) / attempted_swap * 100.0
+
         all_local_acceptance = zeros(commSize)
         all_local_acceptance[rank + 1] = local_acceptance_rate
         MPI.Allgather!(MPI.IN_PLACE, all_local_acceptance, 1, comm)
         all_exchanges = zeros(commSize)
         all_exchanges[rank + 1] = exchange_rate_tot
         MPI.Allgather!(MPI.IN_PLACE, all_exchanges, 1, comm)
+    else
+        commSize = 1
+        rank = 0 
+
+        attempted_swap = 0.0
+        exchange_rate_tot = 0.0
     end
 
     if rank == 0
