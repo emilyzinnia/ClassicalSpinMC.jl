a446ed6f359eab87129fb152e393c8015f7cfa3c
emilyzinnia <ezhang@physics.utoronto.ca>
2024-11-20 15:53:08 -0800

    generalized k point creation

--- src/reciprocal.jl
+++ src/reciprocal.jl
@@ -15,53 +15,56 @@ function reciprocal(a1::T, a2::T, a3::T) where T<:Vector{Float64}
     return (b1, b2, b3) 
 end
 
-""" r is a range of allowed discretized values """
-function get_allowed_wavevectors(uc::UnitCell, r::Array{Float64, 1})
-    steps = collect.([product( tuple( [r for i in 1:length(uc.lattice_vectors)]...)... )...])
-    reciprocal_vectors = reciprocal(uc.lattice_vectors...)
+""" 
+Get allowed wavevectors given the shape of the lattice (tuple). 
+min, max sets the boundaries in reciprocal space in multiples of N in each dimension. 
+min=0, max=1 corresponds to one copy of the FBZ generated based on the reciprocal lattice vectors. 
+"""
+function get_allowed_wavevectors(uc::UnitCell{D}, shape::NTuple{D, Int}; min::Int=0, max::Int=1) where D
+    reciprocal_vectors = reciprocal(uc.lattice_vectors...) 
+    rs = [collect(min*dim:max*dim)/dim for dim in shape] # steps to take along each dimension
+    steps = collect.([product(rs...)...])
     allowed_ks = hcat(reciprocal_vectors...) * hcat(steps...) 
     return allowed_ks
 end
 
-function get_k_plane(uc::UnitCell, L::Int64, 
-    min::Float64=-2.0, max::Float64=2.0)
+#FIXME: untested 
+function get_k_plane(uc::UnitCell{D}, shape::NTuple{D, Int}; min::Int=-2, max::Int=2) where D
     # plane has to be an array of ones and zeros 
-    ks = get_allowed_wavevectors(uc, collect(2*min*L:2*max*L)/L)
+    ks = get_allowed_wavevectors(uc, shape, min=min, max=max)
     temp = copy(ks)
     bounds = broadcast(&, [ 
         collect( round(min*2pi, digits=6) .<= round.(temp[i,:], digits=6) .<= round(max*2pi,digits=6)) 
         for i=1:size(ks)[1] ]...)
     new = temp[:, findall(bounds)]
     return new
-    # get rid of gamma point since transverse projector isn't well defined there 
-    #cond = broadcast(&, [ round.(new[i,:].%pi, digits=5) .!= 0.0 for i=1:size(ks)[1]]...)
-    #return new[:, findall(cond)]
 end
 
-function get_k_path(uc::UnitCell, direction::Array{Float64,1}, L::Int64, 
-                    min::Float64=-2.0, max::Float64=2.0)
-    ks = get_allowed_wavevectors(uc, collect(min*L:max*L)/L)
+function get_k_path(uc::UnitCell{D}, direction::Array{Float64,1}, shape::NTuple{D, Int},
+                    min::Int=-2, max::Int=2) where D
+    ks = get_allowed_wavevectors(uc, shape, min=min, max=max)
     direction ./= norm(direction)
+
     # get all momenta parallel to direction
     ks_ = ks .- direction
     cond = [ round( abs(dot(direction, ks_[:, k]/norm(ks_[:, k])) ), digits=6) == 1.0 for k=1:size(ks)[2] ]
     temp = ks[:,findall(cond)]
+
     # get all momenta within the boundaries specified
     bounds = broadcast(&, [ 
         collect( round(min*2pi, digits=6) .<= round.(temp[i,:], digits=6) .<= round(max*2pi,digits=6)) 
         for i=1:size(ks)[1] ]...)
     new = temp[:, findall(bounds)]
-    
-    # get rid of gamma point since transverse projector isn't well defined there 
-    cond = broadcast(&, [ round.(new[i,:].%pi, digits=5) .!= 0.0 for i=1:size(ks)[1]]...)
-    # return new[:, findall(cond)]
     return new
 end
 
-function get_k_path(UC::UnitCell, hsp::Dict{String, Vector{Float64}}, path::Array{String, 1}, L::Int64)
-    ks = get_allowed_wavevectors(UC, collect(0:L)/L)
+"""
+Get path in momentum space along high symmetry points 
+"""
+function get_k_path(UC::UnitCell{D}, hsp::Dict{String, Vector{Float64}}, path::Array{String, 1}, shape::NTuple{D, Int}) where D
+    ks = get_allowed_wavevectors(UC, shape)
     kpath = hsp[path[1]] 
-    point_count = zeros(Float64, length(path))
+    point_count = zeros(Int64, length(path))   # keep track of the indices where the direction changes 
     path_ = copy(path)
     pop!(path_)
 
