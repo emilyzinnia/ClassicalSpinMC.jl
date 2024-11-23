d009586d466cdf3e9a4272e815bb5fafdc6805aa
emilyzinnia <ezhang@physics.utoronto.ca>
2024-11-22 20:08:40 -0800

    more exports

--- src/ClassicalSpinMC.jl
+++ src/ClassicalSpinMC.jl
@@ -17,7 +17,7 @@ export get_magnetization
 include("helper.jl")
 
 include("hdf5.jl")
-export overwrite_keys!, write_MC_checkpoint, create_params_file, read_lattice
+export overwrite_keys!, write_MC_checkpoint, create_params_file, read_lattice, read_spin_configuration!
 
 include("metropolis.jl")
 export Metropolis, MetropolisAdaptive, MetropolisConstraint, MetropolisConstraintAdaptive
